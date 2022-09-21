423109f8ffc733d47425b661cd7233ef73656bbc
malek mekdashi <malekmekdashi@gmail.com>
2022-09-20 22:30:58 -0700

    clean up

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,31 +1,42 @@
 const fs = require('fs');
-const db = require('../../Develop/db/db.json')
+const db = './db/db.json';
 const router = require('express').Router();
 
 const { v4: uuidv4 } = require('uuid');
 
+let parsedData;
+let newData;
+
 router.get('/api/notes', (req, res) => {
-    fs.readFile(db).then((data) => res.json(JSON.parse(data)))
+    
+
+    fs.readFile(db, (err, data) => {
+        if (err) {console.log(err)
+        
+        parsedData = JSON.parse(data);
+              
+    }});
+    return res.json(parsedData); 
 });
     
 router.post('/api/notes', (req, res) => {
     const updatedNote = {
         id: uuidv4(),
-        title: req.body.type,
+        title: req.body.title,
         text: req.body.text
     };
 
     fs.readFile(db, (err, data) => {
        if (err) throw err;
     
-       const newData = JSON.parse(data);
+       newData = JSON.parse(data);
        newData.push(updatedNote);
-       console.log(newData);
-
-       fs.writeFile(db, JSON.stringify(newData), (err) => { if (err) throw err; res.send('success');
-       });
-
+    fs.writeFile(db, JSON.stringify(newData), (err) => { if (err) throw err;   
+    });
+    
     });
 });
+
+console.log('readFile called');
 
 module.exports = router;
